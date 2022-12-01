d4aeb7d053e1d64a8643dd12367b0e2150ba74bb
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2022-12-01 22:54:45 +0200

    feat: add menu css and js requirements

--- js/menu.js
+++ js/menu.js
@@ -2,15 +2,24 @@
   const refs = {
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
+    backdrop: document.querySelector(".backdrop"),
     menu: document.querySelector(".mob-menu"),
     body: document.querySelector("body"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.backdrop.addEventListener("click", hideModalWindowOnBlur);
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
     refs.body.classList.toggle("no-scroll");
+    refs.backdrop.classList.toggle("is-hidden");
+  }
+
+  function hideModalWindowOnBlur(e) {
+    if (e.target === e.currentTarget) {
+      toggleMenu();
+    }
   }
 })();
