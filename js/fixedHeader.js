74a14672ab6bb93dc2c9a93005176e05c12370eb
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2022-12-04 22:44:57 +0200

    fix: difference with pixel perfect

--- js/fixedHeader.js
+++ js/fixedHeader.js
@@ -1,4 +1,5 @@
 const header = document.querySelector("header");
+const hero = document.querySelector(".hero");
 const sticky = header.offsetTop;
 
 window.onscroll = fixedHeader;
@@ -6,7 +7,9 @@ window.onscroll = fixedHeader;
 function fixedHeader() {
   if (window.pageYOffset > sticky) {
     header.classList.add("header--sticky");
+    hero.classList.add("hero__fixed-header");
   } else {
     header.classList.remove("header--sticky");
+    hero.classList.remove("hero__fixed-header");
   }
 }
